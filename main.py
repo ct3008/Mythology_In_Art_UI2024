68844e3dd981c8240a4411b09b11d41f6c9a4440
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2024-04-12 11:56:12 -0400

    person tracking info

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
 figures = {"1": {
@@ -73,18 +74,32 @@ figures = {"1": {
 }}
 
 
-#		
+#ask: reset for a new user
+
+#time stamp for each page
+user_progress = {}
+
+#sequence of pages visited
+user_sequence = []
+
 @app.route('/')
 def home():
+   user_sequence.append("home")
+   user_progress["home"] = datetime.datetime.now()
+   print(user_sequence)
    return render_template('home.html', figures=figures)
 
 @app.route('/learn')
 def contents():
-   return render_template('contents.html', figures=figures)
+	user_sequence.append("learn")
+	user_progress["learn"] = datetime.datetime.now()
+	return render_template('contents.html', figures=figures)
 
 @app.route('/learn/<id>')
 def learn(id):
-   return render_template('learn.html', figures=figures)
+	user_sequence.append("learn/"+id)
+	user_progress["learn/"+id] = datetime.datetime.now()
+	return render_template('learn.html', figures=figures)
 
 if __name__ == '__main__':
    app.run(debug = True)
